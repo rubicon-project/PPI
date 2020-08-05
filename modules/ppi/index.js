c19ef367d933f192cc61ede511bc64fe9d49f937
Filip Stamenkovic <ficadub@gmail.com>
2020-08-05 13:05:57 +0200

    comment step by step

--- modules/ppi/index.js
+++ modules/ppi/index.js
@@ -5,6 +5,11 @@ import { getGlobal } from '../../src/prebidGlobal.js';
  * @param {(Object[])} transactionObjects array of adUnit codes to refresh.
  */
 export function requestBids(transactionObjects) {
+  // validate each transactionObject, log faulty one, use validated
+  // group transactionObjects by source-destination pair.
+  // for each transactionObject match adUnit pattern
+  // for each matched adUnit pattern construct adUnits and hold hb auction (if source is not cache) and send result to destination module
+  // for each matched adUnit pattern get cached bids and send them to destination module (if source is cache)
 }
 
 (getGlobal()).ppi = {
