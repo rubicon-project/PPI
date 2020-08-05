b8fe006b08993f428419497a01f954ae3e911ec0
aleksatr <aleksatr@gmail.com>
2020-08-05 14:56:44 +0200

    AUP matching

--- modules/ppi/index.js
+++ modules/ppi/index.js
@@ -23,10 +23,14 @@ export function requestBids(transactionObjects) {
   for (const source in groupedTransactionObjects) {
     for (const dest in groupedTransactionObjects[source]) {
       groupedTransactionObjects[source][dest].forEach((transactionObject) => {
-        aups.push(dest);
-        aups = [];
-
-        transactionResult.push(createTransactionResult(transactionObject, {}));
+        let aup = findMatchingAUP(to, adUnitPatterns);
+        if (aup) {
+          aups.push(aup);
+        } else {
+          console.log("[PPI] No AUP matched for transaction object", transactionObject);
+        }
+        
+        transactionResult.push(createTransactionResult(transactionObject, aup));
       });
     }
   }
@@ -53,21 +57,78 @@ export function groupTransactionObjects(transactionObjects) {
 }
 
 function createTransactionResult(transactionObject, adUnitPattern) {
+  let aup;
+  if (adUnitPattern) {
+    aup = {
+      divPattern: adUnitPattern.divPattern.toString(),
+      slotPattern: adUnitPattern.slotPattern.toString(),
+    };
+  }
+
   return {
     name: transactionObject.name,
     type: transactionObject.type,
     hbSource: transactionObject.hbSource,
     hbDestination: transactionObject.hbDestination,
     match: {
-      status: true,
-      aup: {
-        divPattern: adUnitPattern.divPattern,
-        slotPattern: adUnitPattern.slotPattern,
-      }
+      status: !!aup,
+      aup: aup
     }
   };
 }
 
+function findMatchingAUP(transactionObject, adUnitPatterns) {
+  return adUnitPatterns.find(aup => {
+    switch(transactionObject.type) {
+      case "slotPattern":
+        if (!aup.slotPattern) {
+          break;
+        }  
+      
+        // 'transactionObject.name' should be renamed
+        // TODO: create new RegExp() out of regex strings
+        return aup.slotPattern.test(transactionObject.name);
+      case "divPattern":
+        if (!aup.divPattern) {
+          break;
+        }  
+      
+        // 'transactionObject.name' should be renamed
+        // TODO: create new RegExp() out of regex strings
+        return aup.divPattern.test(transactionObject.name);
+      case "gptSlotObject":
+        // NOTICE: gptSlotObjects -> gptSlotObject, in this demo we assume single gpt slot object per transaction object
+        // we also assume that `transactionObject.name` carries the gpt slot object
+        let match = true;
+        if (aup.slotPattern) {
+          match = aup.slotPattern.test(transactionObject.name.getAdUnitPath());
+        }
+        if (aup.divPattern) {
+          match = match && aup.divPattern.test(transactionObject.name.getSlotElementId());
+        }
+
+        // AUP validation should guarantee that AUP has at least one pattern (div or slot)
+        return match;
+      default:
+        // this should never happen
+        // if transaction object passed validation
+        console.log("[PPI] Invalid transaction object type", transactionObject.type)
+    }
+
+    return false;
+  });
+}
+
+const adUnitPatterns = [];
+
 (getGlobal()).ppi = {
   refreshBids: requestBids,
+  adUnitPatterns: adUnitPatterns,
 };
+
+
+// ------------------------
+// Questions:
+//  - aup slot/div pattern matches against transaction object pattern? Both are patterns, shouldn't transaction object have concrete values?
+//  - transaction object of type 'gptSlotObjects' should recieve array of gpt slot objects, how? do we match one aup per passed gpt slot object? can we then break it one gpt slot object per transaction object?
+
