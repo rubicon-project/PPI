dba1030f4f163ef1e8cd8842bb3655bc693482b3
Michael Moschovas <michael.moschovas@yahoo.com>
2021-04-13 14:28:29 -0400

    Repushing last

--- modules/ppi/hbInventory/aup/aup.js
+++ modules/ppi/hbInventory/aup/aup.js
@@ -108,7 +108,6 @@ export function createAdUnit(adUnitPattern, transactionObject) {
     aupSizes = aupSizes.filter(s => Array.isArray(s) && s.length === 2);
     
     if (utils.deepAccess(adUnit, 'mediaTypes.banner')) utils.deepSetValue(adUnit, 'mediaTypes.banner.sizes', sortSizes(aupSizes));
-    utils.deepSetValue(adUnit, 'mediaTypes.banner.sizes', sortSizes(aupSizes));
 
     // if aup code was not published, generate one
     if (!adUnit.code) {
